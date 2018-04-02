e4c633b84cb67bc13bfccb89d8547c97d6867e72
Matt Conway <matt@conwaysplace.com>
2018-04-02 12:34:34 -0400

    add otp helper command

---
+++ lib/atmos/commands/otp.rb
@@ -0,0 +1,54 @@
+require 'atmos/commands/base_command'
+require 'atmos/otp'
+require 'clipboard'
+
+module Atmos::Commands
+
+  class Otp < BaseCommand
+
+    def self.description
+      "Generates an otp token for the given user"
+    end
+
+    option ["-s", "--secret"],
+           'SECRET', "The otp secret\nWill save for future use"
+
+    option ["-c", "--clipboard"],
+           :flag,
+           <<~EOF
+            Automatically copy the token to the system
+            clipboard.  For dependencies see:
+            https://github.com/janlelis/clipboard
+            EOF
+
+    parameter "NAME",
+              "The otp name (IAM username)"
+
+    def execute
+      code = nil
+      if secret
+        Atmos::Otp.instance.add(name, secret)
+        code = Atmos::Otp.instance.generate(name)
+        Atmos::Otp.instance.save
+      else
+        code = Atmos::Otp.instance.generate(name)
+      end
+
+      if code.nil?
+        signal_usage_error <<~EOF
+          No otp secret has been setup for #{name}
+          Use the -m flag to 'atmos user create' to create/activate one
+          or associate an existing secret with 'atmos otp -s <secret> <name>'
+        EOF
+      else
+        puts code
+      end
+
+      if clipboard?
+        Clipboard.copy(code)
+      end
+    end
+
+  end
+
+end
---
+++ spec/commands/otp_spec.rb
@@ -0,0 +1,53 @@
+require "atmos/commands/otp"
+
+describe Atmos::Commands::Otp do
+
+  let(:cli) { described_class.new("") }
+
+  around(:each) do |ex|
+    within_construct do |c|
+      c.file('config/atmos.yml')
+      Atmos.config = Atmos::Config.new("ops")
+      ex.run
+      Atmos.config = nil
+    end
+  end
+
+  describe "--help" do
+
+    it "produces help text under standard width" do
+      expect(cli.help).to be_line_width_for_cli
+    end
+
+  end
+
+  describe "execute" do
+
+    before(:each) do
+      Atmos::Otp.instance_variable_set(:@singleton__instance__, nil)
+    end
+
+    it "fails for invalid name" do
+      expect{cli.run(["newkey"])}.to raise_error(Clamp::UsageError, /No otp secret/)
+    end
+
+    it "works for existing name" do
+      Atmos::Otp.instance.add("newkey", "secret")
+      expect(Clipboard).to_not receive(:copy)
+      expect{cli.run(["newkey"])}.to output(/\d{6}/).to_stdout
+    end
+
+    it "can copy to clipboard for existing name" do
+      Atmos::Otp.instance.add("newkey", "secret")
+      expect(Clipboard).to receive(:copy)
+      expect{cli.run(["-c", "newkey"])}.to output(/\d{6}/).to_stdout
+    end
+
+    it "can save given key" do
+      expect(Atmos::Otp.instance).to receive(:save)
+      expect{cli.run(["-s", "sekret", "newkey"])}.to output(/\d{6}/).to_stdout
+    end
+
+  end
+
+end
