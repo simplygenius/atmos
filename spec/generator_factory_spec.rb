c463824f8c643da49de56e5dbe0a78b48eb10942
Matt Conway <matt@conwaysplace.com>
2018-04-11 14:38:29 -0400

    fix git repo fixture

--- spec/generator_factory_spec.rb
+++ spec/generator_factory_spec.rb
@@ -1,4 +1,5 @@
 require 'atmos/generator_factory'
+require 'zip'
 
 describe Atmos::GeneratorFactory do
 
@@ -6,6 +7,19 @@ describe Atmos::GeneratorFactory do
 
   let(:gen) { described_class.new([], quiet: true) }
 
+  def git_repo_fixture
+    tmpdir = Dir.mktmpdir("git-repo-fixture-")
+    at_exit { FileUtils.remove_entry(tmpdir) }
+    open("#{fixture_dir}/template_repo.git.zip", 'rb') do |io|
+      Zip::File.open_buffer(io) do |zip_file|
+        zip_file.each do |f|
+          fpath = File.join(tmpdir, f.name)
+          f.extract(fpath)
+        end
+      end
+    end
+    return "#{tmpdir}/template_repo.git"
+  end
 
   describe "expand_sourcepaths", :vcr do
 
@@ -27,14 +41,16 @@ describe Atmos::GeneratorFactory do
     end
 
     it "expands a git archive locally" do
-      expanded = described_class.expand_sourcepaths(["#{fixture_dir}/template_repo.git"])
+      repo_dir = git_repo_fixture
+      expanded = described_class.expand_sourcepaths([repo_dir])
       expect(expanded.size).to eq(1)
       expect(expanded.first).to match(/^\/.*/)
       expect(Dir.exist?(expanded.first)).to be true
     end
 
     it "uses subdir from a git archive" do
-      expanded = described_class.expand_sourcepaths(["#{fixture_dir}/template_repo.git#subdir"])
+      repo_dir = git_repo_fixture
+      expanded = described_class.expand_sourcepaths(["#{repo_dir}#subdir"])
       expect(expanded.size).to eq(1)
       expect(expanded.first).to match(/^\/.*/)
       expect(expanded.first).to match(/subdir$/)
